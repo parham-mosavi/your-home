2f331e8f62f22dd6d0f480d274bbc06cad7a4ee3
parham-mosavi <pa12.ha.34.o@gmail.com>
2025-12-28 18:41:59 +0330

    add guard for logged in user

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -12,15 +12,16 @@ import { BuihomeComponent } from './components/buihome/buihome.component';
 import { RegistertecComponent } from './components/accunttec/registertec/registertec.component';
 import { LogintecComponent } from './components/accunttec/logintec/logintec.component';
 import { DorehiComponent } from './components/dorehi/dorehi.component';
+import { authLoggedInGuard } from './guards/auth-logged-in.guard';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     // { path: 'home', component: HomeComponent },
     // { path: 'members/member-card', component: MemberCardComponent },
-    { path: 'account/buildingregister', component: RegisterComponent },
-    { path: 'account/loginbuilding', component: LoginComponent },
-    { path: 'account/tecregister', component: RegistertecComponent },
-    { path: 'account/logintec', component: LogintecComponent },
+    { path: 'account/buildingregister', component: RegisterComponent, canActivate: [authLoggedInGuard] },
+    { path: 'account/loginbuilding', component: LoginComponent, canActivate: [authLoggedInGuard] },
+    { path: 'account/tecregister', component: RegistertecComponent, canActivate: [authLoggedInGuard] },
+    { path: 'account/logintec', component: LogintecComponent, canActivate: [authLoggedInGuard] },
     { path: 'footer', component: FooterComponent },
     { path: 'navbar', component: NavbarComponent },
     { path: 'members/member', component: MembersComponent },
